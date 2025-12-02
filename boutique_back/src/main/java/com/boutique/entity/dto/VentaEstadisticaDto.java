e8f4b667ae4d59d6999f32f6e654dea3cba9051e
Gerson <villarroelpoma2@gmail.com>
2025-12-02 19:12:15 -0400

    DTO para estadística de venta por mes

---
+++ boutique_back/src/main/java/com/boutique/entity/dto/VentaEstadisticaDto.java
@@ -0,0 +1,11 @@
+package com.boutique.entity.dto;
+
+/**
+ * @author GERSON
+ */
+
+public interface VentaEstadisticaDto {
+    Integer getAnio();
+    Integer getMes();
+    Long getTotalVentas();
+}
